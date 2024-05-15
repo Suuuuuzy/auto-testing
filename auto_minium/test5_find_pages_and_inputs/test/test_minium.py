24ad1011e9afe105591cef7916e8cf1bd5c0570a
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-05-14 20:48:04 -0400

    add forms in test_minium, ignore buttons for now

--- auto_minium/test5_find_pages_and_inputs/test/test_minium.py
+++ auto_minium/test5_find_pages_and_inputs/test/test_minium.py
@@ -9,7 +9,7 @@ class Minium_Query(BaseDef):
 
         for page in pages:
             self.open_route("/" + page)
-            print(page)
+            # print(page)
             # self.page.wait_for(1)
             # 1. inputs
             inputs = self.find_all_inputs()
@@ -17,44 +17,41 @@ class Minium_Query(BaseDef):
             for input_block in inputs:
                 try:
                     input_block.input(text_input, with_confirm=True)
+                    self.logger.info(f'action: input something in input in page: {page}')
                 except Exception as e:
                     print(f'encountering error during query: {e}')
                 # self.screen_shot_save("(" + page  + ")")
                 # self.page.wait_for(5)
                 value = input_block.attribute("value")[0]
-                try: 
-                    self.assertEqual(text_input, value, f"element {input_block} is properly modified")
-                except AssertionError as e:
-                    print(f"failed assertion, expected input {text_input}, but gets {value}")
+                # try: 
+                #     self.assertEqual(text_input, value, f"element {input_block} is properly modified")
+                # except AssertionError as e:
+                #     print(f"failed assertion, expected input {text_input}, but gets {value}")
                 # if page!=self.app.get_current_page().path:
                 #     print('page changes after input, navigating back')
                 #     self.app.navigate_back()
+            
             # 2. forms
             forms = self.find_all_forms()
-            print(f"there are {len(forms)} forms on page {page}")
+            self.logger.info(f"there are {len(forms)} forms on page {page}")
             for form_block in forms:
                 try:
                     inputElements = self.find_all_inputs_from_component(form_block)
-                    inputArrays = {};
+                    inputArrays = {}
                     for inputElement in inputElements:
-                        inputArrays[inputElement.property("name")] = text_input;
+                        inputArrays[inputElement.name] = text_input
                     form_block.trigger("submit", {"value" : inputArrays})
+                    self.logger.info(f'action: submit a form in {form_block} page: {page}')
                 except Exception as e:
-                    print(f'encountering error during query: {e}')
-                # self.page.wait_for(5)
-                try: 
-                    self.assertEqual(text_input, "string", "element is properly modified")
-                except AssertionError as e:
-                    print(f"failed assertion, expected input {text_input}")
-                # if page!=self.app.get_current_page().path:
-                #     print('page changes after form submit, navigating back')
-                #     self.app.navigate_back()
+                    self.logger.info(f'encountering error during query: {e}')
+            
             # 3. buttons
-            btns = self.find_all_buttons()
-            print(f"there are {len(btns)} buttons on page {page}")
-            for btn in btns:
-                btn.tap()
-                self.page.wait_for(1)
+            # btns = self.find_all_buttons()
+            # print(f"there are {len(btns)} buttons on page {page}")
+            # for btn in btns:
+            #     btn.tap()
+            #     self.page.wait_for(1)
+            #     self.logger.info(f'action: tap a button in page: {page}')
                 # if page!=self.app.get_current_page().path:
                 #     self.app.navigate_back()
             # this is to avoid page stack overflow
