8835f8ae92a3a8647e53974c0cdea17f63e88195
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-05-17 13:30:21 -0400

    add config for processing data on mac

--- miniapp_data/utils/config.py
+++ miniapp_data/utils/config.py
@@ -1,3 +1,4 @@
 wxapkg_ROOT = ""
+mac_unpacked_wxapkg_ROOT = "/Users/jianjia/Documents/projects/mini-app/WeMinT_dataset/groundtruth/miniprograms"
 unpacked_wxapkg_ROOT = "/media/data4/jianjia_data4/miniapp_data/WeMinT_dataset/groundtruth/miniprograms"
 org_groundtruth_ROOT = "/media/data4/jianjia_data4/miniapp_data/WeMinT_dataset/groundtruth_org/groundtruth/miniprograms"
