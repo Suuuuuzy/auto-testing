906b2c90d60318fdaaa5ad7e11741dc5e31d35e2
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-05-17 16:56:41 -0400

    add system check for utils pipeline and main

--- miniapp_data/utils/revert_from_original_data.py
+++ miniapp_data/utils/revert_from_original_data.py
@@ -1,17 +1,18 @@
 import os
 from config import *
 import shutil
+import platform
 
 def copyanything(src, dst):
-    try:
-        if os.path.exists(dst):
-            shutil.rmtree(dst)
-        shutil.copytree(src, dst)
-    except:
-        print('failed')
+    # try:
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    shutil.copytree(src, dst)
+    # except:
+        # print('failed')
         # raise
         
-def main(appid):
+def main(appid, org_groundtruth_ROOT, unpacked_wxapkg_ROOT):
     src = os.path.join(org_groundtruth_ROOT, appid)
     if not os.path.exists(src):
         print('src does not exist')
@@ -20,5 +21,10 @@ def main(appid):
     copyanything(src, dst)
 
 if __name__ == '__main__':
-    appid = "wx7fbb70a732424777"
-    main(appid)
+    appid = "wx4b1dfa2b8f503e6f"
+    if platform.system()=="Linux": #Linux
+        main(appid, org_groundtruth_ROOT, unpacked_wxapkg_ROOT)
+    else: #Darwin
+        main(appid, mac_org_groundtruth_ROOT, mac_unpacked_wxapkg_ROOT)
+   
+    
