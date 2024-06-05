a88da3c50a5886e49a68a15c0f8f501c0a6b9751
bella-xia <zxia15@jhu.edu>
2024-06-05 16:27:26 -0400

    modified json format

--- auto_minium/test11_check_miniprogram_coverage_specifics/minium_tests/base_taint.py
+++ auto_minium/test11_check_miniprogram_coverage_specifics/minium_tests/base_taint.py
@@ -1,5 +1,5 @@
 import minium_tests.base_case as base_case
-import minium
+import minium, json
 from collections import namedtuple
 from typing import Dict, List
 
@@ -22,12 +22,18 @@ class BaseTaint(base_case.BaseCase):
     """
     封装公用页面基础操作方法
     """
+    
     def get_element_using_selector(self, selector_info):
         return self.page.get_element(
             selector=selector_info.selector, 
             inner_text=selector_info.inner_text, 
             text_contains=selector_info.text_contains, 
             value=selector_info.value)
+
+    def get_json_data(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            json_data = file.read()
+        return json.loads(json_data)
 
     def input_text_helper(self, default_text, class_name):
         POTENTIAL_IDENTIFIER_AND_INPUT = {'电话' : '13880000000',
@@ -145,45 +151,28 @@ class BaseTaint(base_case.BaseCase):
         return return_arr
 
     def hook_wx_methods_with_page_defined_method_call(
-        self,
-        wx_methods: List[str],
-        page_defined_method: str,
+        self, page: str, wx_methods: List[str], page_defined_method: str,
         method_args: Dict[str, any]
     ) -> Dict[str, any]:
+        
+        if (self.app.get_current_page() != page):
+            print(f"currently at page {self.app.get_current_page()}, redirecting to {page}")
+            self.open_route("/" + page)
         
         callback_arr = {}
         return_arr = {}
         for wx_method in wx_methods:
-            # callback_arr[wx_method] = {}
-            # callback_arr[wx_method]['before'] = minium.Callback()
-            # callback_arr[wx_method]['after'] = minium.Callback()
             callback_arr[wx_method] = minium.Callback()
-            # print('callback identified')
-            # print(callback_arr[wx_method]['before'], callback_arr[wx_method]['after'],  callback_arr[wx_method]['callback'])
             self.app.hook_wx_method(wx_method, callback=callback_arr[wx_method])
-        # callback = minium.Callback()
-        # self.app.hook_wx_method('chooseInvoiceTitle', callback=callback)
-        # element.tap()
+
         self.app.current_page.call_method(page_defined_method, [method_args])
 
-        # self.app.call_wx_method('chooseInvoiceTitle', args={"success": None})
-
-        # if callback.wait_called(timeout=10) is True:
-        #     return_arr = callback.get_callback_result()
-        #     print('got it!')
-        # else: 
-        #     return_arr = None
-        #     print('not very workinggggg')
-
         for wx_method in wx_methods:
-            # if callback_arr[wx_method]['before'].wait_called(timeout=10) is True and callback_arr[wx_method]['after'].wait_called(timeout=10) is True and callback_arr[wx_method]['callback'].wait_called(timeout=10) is True:
             if callback_arr[wx_method].wait_called(timeout=10) is True:
                 return_arr[wx_method] = callback_arr[wx_method].get_callback_result()
             else:
                 return_arr[wx_method] = None
             self.app.release_hook_wx_method(wx_method)
-        # is_called = called.acquire(timeout=10)
-        # self.app.release_hook_wx_method('chooseInvoiceTitle')
 
         return return_arr
 
@@ -203,7 +192,6 @@ class BaseTaint(base_case.BaseCase):
         Returns:
         List[Dict] : a list of all the instances of wx api methods passed args
         """
-        self.open_route(page)
         callback_arr = {}
         for wx_method in wx_methods:
             callback_arr[wx_method] = minium.Callback()
