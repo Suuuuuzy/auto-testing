475d4536866f21a6f86ad029642fb93332281e55
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-06-10 21:39:53 -0400

    update log cg checking and preprocessing

--- miniapp_data/utils/pipeline_utils/modify_files.py
+++ miniapp_data/utils/pipeline_utils/modify_files.py
@@ -3,6 +3,8 @@ from pipeline_utils.solve_fail import solve_fail
 from pipeline_utils.modify_wxml import format_wxml_style_attribute
 from pipeline_utils.modify_wxs import format_wxs_style_attribute
 from pipeline_utils.modify_app_json import check_borderStyle, check_window_attrs, check_attrs, read_json_file, write_json_file
+import logging
+logger = logging.getLogger(__name__)
 
 def find_and_format_files(directory):
     for root, dirs, files in os.walk(directory):
