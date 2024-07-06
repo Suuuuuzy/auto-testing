1ad7e3feedd947b996a80b3998875d7a21cbcd97
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-07-05 20:55:20 -0400

    iterate methods in each page

---
+++ auto_minium/method_iterator/test/bind_func_arguments.py
@@ -0,0 +1,127 @@
+text_input = """<script\x0Atype="text/javascript">javascript:alert("minium");</script>"""
+
+# catchtap
+bindtap_arg = {
+                  "type":"tap",
+                  "timeStamp":99793,
+                  "target":{"id":"","offsetLeft":0,"offsetTop":596,"dataset":{}},
+                  "currentTarget":{"id":"","offsetLeft":0,"offsetTop":596,"dataset":{}},
+                  "mark":{},
+                  "detail":{"x":213.546875,"y":622.328125},
+                  "touches":[{"identifier":0,"pageX":213.546875,"pageY":622.328125,"clientX":213.546875,"clientY":116.328125,"force":1}],
+                  "changedTouches":[{"identifier":0,"pageX":213.546875,"pageY":622.328125,"clientX":213.546875,"clientY":116.328125,"force":1}],
+                  "mut":False,
+                  "_userTap":True
+                  }
+
+bindblur_arg = {
+    "type":"blur",
+    "timeStamp":29914,
+    "target":{"id":"","offsetLeft":0,"offsetTop":126,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":0,"offsetTop":126,"dataset":{}},
+    "mark":{},
+    "detail":{"value":text_input,"cursor":14},
+    "mut":False,
+    "_userTap":False
+    }
+
+bindinput_arg = {
+    "type":"input",
+    "timeStamp":1719778404943,
+    "detail":{"value":text_input,"cursor":7,"keyCode":86},
+    "target":{"id":"","dataset":{},"offsetTop":18,"offsetLeft":32},
+    "currentTarget":{"id":"","dataset":{},"offsetTop":18,"offsetLeft":32},
+    "touches":[]
+    }
+
+bindsubmit_arg = {
+    "type":"submit",
+    "timeStamp":58596,
+    "target":{"id":"","offsetLeft":0,"offsetTop":642,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":0,"offsetTop":642,"dataset":{}},
+    "mark":{},
+    "detail":{"value":{"input":"","textarea":"","textarea2":""},"target":{"id":"","dataset":{},"offsetTop":964,"offsetLeft":0}},
+    "mut":False,
+    "_userTap":True
+    }
+
+bindconfirm_arg =  {
+    "type":"confirm",
+    "timeStamp":4418837,
+    "target":{"id":"","offsetLeft":32,"offsetTop":18,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":32,"offsetTop":18,"dataset":{}},
+    "mark":{},
+    "detail":{"value":text_input},
+    "mut":False,
+    "_userTap":False
+    }
+
+bindlongtap_arg =  {
+    "type":"longtap",
+    "timeStamp":153385.19999980927,
+    "target":{"id":"","offsetLeft":96,"offsetTop":514,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":96,"offsetTop":514,"dataset":{}},
+    "touches":[{"identifier":"0","x":60.25,"y":24.515625}],
+    "changedTouches":[{"identifier":"0","x":60.25,"y":24.515625}]
+    }
+
+# for buttons, we can find the elements and tap tho
+# bindlongtap
+# bindconfirm
+reset_arg =  {
+    "type":"reset",
+    "timeStamp":13093,
+    "target":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "mark":{},
+    "detail":{"target":{"id":"","dataset":{},"offsetTop":336,"offsetLeft":0}},
+    "mut":False,
+    "_userTap":True
+    }
+
+
+bindtouchstart_arg = {
+    "type":"touchstart",
+    "timeStamp":49921.199999809265,
+    "target":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "touches":[{"identifier":0,"x":150.46875,"y":92}],
+    "changedTouches":[{"identifier":0,"x":150.46875,"y":92}]
+    }
+
+bindtouchmove_arg = {
+    "type":"touchmove",
+    "timeStamp":273127.80000019073,
+    "target":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "touches":[{"identifier":0,"x":143.74224853515625,"y":89.68048095703125}],
+    "changedTouches":[{"identifier":0,"x":143.74224853515625,"y":89.68048095703125}]
+    }
+
+bindtouchend_arg = {
+    "type":"touchend",
+    "timeStamp":273161.5,
+    "target":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "currentTarget":{"id":"","offsetLeft":0,"offsetTop":0,"dataset":{}},
+    "touches":[],
+    "changedTouches":[{"identifier":0,"x":148.65960693359375,"y":85.210205078125}]
+    }
+
+# bindchange
+# bindgetuserinfo
+
+
+trigger_arg_dic = {
+                "bindtap": bindtap_arg,
+                "catchtap": bindtap_arg,
+                "bindblur": bindblur_arg,
+                "bindinput": bindinput_arg,
+                "bindsubmit": bindsubmit_arg,
+                "catchsubmit": bindsubmit_arg,
+                "bindconfirm": bindconfirm_arg,
+                "bindlongtap": bindlongtap_arg,
+                "bindtouchstart": bindtouchstart_arg,
+                "bindtouchmove": bindtouchmove_arg,
+                "catchtouchmove": bindtouchmove_arg,
+                "bindtouchend": bindtouchend_arg
+                }
---
+++ auto_minium/method_iterator/test/test_minium.py
@@ -0,0 +1,118 @@
+from basedef import BaseDef
+import os, json, time
+from bind_func_arguments import *
+import logging
+logger_main = logging.getLogger(__name__)
+logging.basicConfig(
+    filename='autominium_test.log',
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+
+class Minium_Query(BaseDef):
+    def setUp(self):
+        appid = self.mini.project_path.split('/')[-1]
+        # __setLog__('/home/suzy/temp/new_taint_log_file/'+appid)
+        result = self.app.evaluate(
+            "function(){args=arguments;__setLog__('/home/suzy/temp/new_taint_log_file/'+args[0])}",[appid], sync=True
+        )
+        
+    def test_methods(self):
+        text_input = """<script\x0Atype="text/javascript">javascript:alert("minium");</script>"""
+        # try to find bind_methods_navi.json first
+        bind_json_file = os.path.join(self.mini.project_path, "bind_methods_navi.json")
+        # if it does not exist, use bind_methods.json
+        if not os.path.exists(bind_json_file):
+            bind_json_file = os.path.join(self.mini.project_path, "bind_methods.json")
+        with open(bind_json_file) as f:
+            bind_methods = json.load(f)
+        pages = self.find_all_pages()
+        pages = ['/' + i for i in pages]
+        finished_pages = set()
+        
+        def get_arg(trigger, item):
+            if trigger not in ["bindsubmit"]:
+                return trigger_arg_dic[trigger]
+            elif trigger == "bindsubmit":
+                arg = trigger_arg_dic[trigger]
+                form_data = item["form_dic"]
+                for key in form_data:
+                    form_data[key] = text_input 
+                arg["detail"]["value"] = form_data
+                return arg
+                
+        def dealWithPage(page):
+            page_in_json = page[1:]
+            if page in finished_pages:
+                return
+            bindings_cnt = 0
+            for trigger in bind_methods[page_in_json]["binding_event"]:
+                # if trigger not in ["bindsubmit"]:
+                bindings_cnt += len(bind_methods[page_in_json]["binding_event"][trigger])
+            if bindings_cnt==0:
+                finished_pages.add(page)
+                pages.remove(page)
+                self.logger.info(f'[+] {page} finishes')
+                return
+            
+            # we want to do bindsubmit, bindinput, bindconfirm first? then bindtap, bindblur?
+            triggers = [i for i in bind_methods[page_in_json]["binding_event"]]
+            prioritized_triggers = ["bindinput", "bindconfirm", "bindsubmit"]
+            # if trigger in prioritized_triggers exists in triggers, move them to the front
+            for trigger in prioritized_triggers:
+                if trigger in triggers:
+                    triggers.remove(trigger)
+                    triggers.insert(0, trigger)
+            self.logger.info(f'[+] See triggers {triggers}')
+            for trigger in triggers:
+                if trigger not in trigger_arg_dic:
+                    logger_main.error(f"[+] {trigger} not implemeted in trigger_arg_dic in miniapp {self.mini.project_path}!")
+                    # skip the ones we don't include for now, to see whether the test stops
+                    bind_methods[page_in_json]["binding_event"][trigger] = []
+                    continue
+                items = bind_methods[page_in_json]["binding_event"][trigger]
+                while len(items)>0:
+                    item = items[0]
+                    try:
+                        self.logger.info(f'[+] Try calling method {item["handler"]} in page {page}, current page: {self.app.get_current_page()}')
+                        if page!=self.app.get_current_page().path:
+                            self.logger.error(f'[+] Calling method in different pages')
+                            stack = self.app.get_page_stack() 
+                            self.logger.error(f'[+] Current stack: {stack}')
+                        result  = self.page.call_method(item["handler"], get_arg(trigger, item))
+                        self.logger.info(f'[+] Call method {item["handler"]}, result: {result}')
+                    except:
+                        self.logger.info(f'[+] Call method error {item["handler"]}')
+                        logger_main.error(f'[+] Call method error {item["handler"]} in miniapp {self.mini.project_path}')
+                    items.remove(item)
+                    self.logger.info(f'[+] There are {len(items)} methods for {trigger} left on page: {page}')
+                    time.sleep(2) # wait until the call_method takes effect (if the call method is navigation)
+                    cur_page = self.app.get_current_page()
+                    cur_path = cur_page.path
+                    self.logger.info(f'[+] Check currnet page: {cur_page}')
+                    if page!=cur_path:
+                        self.logger.info(f'[+] Navigate naturally, from {page} to {cur_path}')
+                        # when this returns, we assume the page is still here, but it's not the case!
+                        dealWithPage(cur_path)
+                        return
+           
+                    
+        
+        while len(pages)>0:
+            page = pages[0]
+            stack = self.app.get_page_stack() 
+            if len(stack)>9:
+                self.relaunch_to_open(page)
+            else:
+                self.navigate_to_open(page)
+            self.logger.info(f'[+] Navigate through API, to: {page}')
+            time.sleep(2) # wait until navigation takes effect
+            dealWithPage(page)
+        
+        
+    def tearDown(self):
+        self.mini.shutdown()
+        super().tearDown()
+        
