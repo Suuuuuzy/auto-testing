2f98edcb415a77a5c14d2849657e626dc3eabcb4
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-08-11 16:22:12 -0400

    update result analysis and random sampling

--- miniapp_data/utils/check_logs/random_sample.py
+++ miniapp_data/utils/check_logs/random_sample.py
@@ -1,3 +1,4 @@
+#!/media/dataj/wechat-devtools-linux/testing/myenv/bin/python
 import os, re, json
 from get_appids import get_appids
 import argparse
@@ -6,14 +7,12 @@ import random
 
 def main():
     parser = argparse.ArgumentParser(description='Get error miniapps')
-    # parser.add_argument('--large_scale', action='store_true', help="large_scale")    
     parser.add_argument('--cmrf_fp', action='store_true',  help="cmrf_fp")
     parser.add_argument('--cmrf_fn', action='store_true',  help="cmrf_fn")
     parser.add_argument('--file', '-f',  help="preprocess the ids in the file")
     args = parser.parse_args()
     
     pkgs = None
-    
     if args.cmrf_fp:
         output_dir = '/media/dataj/miniapp_data/CMRF_groundtruth/fp_dataset_unpack/'
         pattern = r'fp_dataset_unpack/(wx[a-zA-Z0-9]{16})'
@@ -61,11 +60,26 @@ def main():
         unpacked_files = set(pkgs)
     else:
         unpacked_files = set(os.listdir(output_dir))
-    
     error_appids = error_appids & unpacked_files
     no_error_appids = no_error_appids & unpacked_files
+    run_appids = run_appids & unpacked_files
+    # see what reasons cause the errors
+    no_appjson = set()
+    for i in tqdm(run_appids):
+        files = os.listdir(os.path.join(output_dir, i))
+        if "app.json" not in files:
+            no_appjson.add(i)
+    print(f'{len(no_appjson)} of tested miniapps do not have app.json')
+    error_appids = error_appids-no_appjson
+    no_error_appids = no_error_appids-no_appjson
     
+    wxpay_appids = set(get_appids('wxpay'))
+    print(f'Among the error appids, {len(error_appids.intersection(wxpay_appids))} of them have wxpay patterns')
+    error_appids = error_appids - wxpay_appids
+    print(f'Error appids without wxpay patterns/lack of app.json: {len(error_appids)}')
+    no_error_appids = no_error_appids - wxpay_appids
     error_appids = error_appids - no_error_appids
+    
     print(f'Error appids: {len(error_appids)}')
     print(f'Failures appids: {len(failure_appids)}')
     print(f'No error appids: {len(no_error_appids)}')
@@ -79,38 +93,39 @@ def main():
     print(f'log in Failures appids: {len(failure_appids.intersection(log_appids))}')
     print(f'log in No error appids: {len(no_error_appids.intersection(log_appids))}')
     
-    # randomly select 100 from no_error_appids
-    random_100 = random.sample(no_error_appids, 100)
+    return error_appids, no_error_appids, failure_appids, run_appids
+
+def output_appids(appids, filename):
+    with open(f"/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/appid_file/{filename}.json", "w") as f:
+        json.dump(appids, f, indent = 2)
+
+def sample_from_appids(appids, num):
+    random_100 = random.sample(appids, num)
     random_100_dic = {}
     for i in random_100:
         tmp_logs = [log for log in logs if log.startswith(i)]
         random_100_dic[i] = tmp_logs
     save_dic = {"pkgpath":"","unpackpath":"/media/dataj/miniapp_data/wxapkgs-42w-unpacked", "pkgs":random_100, "appid_log_dic":random_100_dic}
-    with open("/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/appid_file/random_100_no_error_appids.json", "w") as f:
+    with open(f"/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/appid_file/random_{num}_no_error_appids.json", "w") as f:
         json.dump(save_dic, f, indent = 2)
-    
-    # see what reasons cause the errors
-    error_no_appjson = set()
-    for i in tqdm(error_appids):
-        files = os.listdir(os.path.join(output_dir, i))
-        if "app.json" not in files:
-            error_no_appjson.add(i)
-    print(f'Among the error appids, {len(error_no_appjson)} of them are due to lack of app.json')
-    error_appids = error_appids-error_no_appjson
-
-    wxpay_appids = set(get_appids('wxpay'))
-    print(f'Among the error appids, {len(error_appids.intersection(wxpay_appids))} of them have wxpay patterns')
-    error_appids = error_appids - wxpay_appids
-    print(f'Error appids without wxpay patterns/lack of app.json: {len(error_appids)}')
-    
-    error_appids = list(error_appids)
-    # we need to run the appids in the output file again, 
-    # so the final writen error ones are the ones with app.json and not wxpay
-    with open(output_file, "w") as f:
-        json.dump(error_appids, f, indent = 2)
-
+        
+def update_log(num):
+    log_path = "/home/suzy/temp/decoded_new_taint_log_file"
+    logs = os.listdir(log_path)
+    random_100_dic = {}
+    with open(f"/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/appid_file/random_{num}_no_error_appids.json") as f:
+        save_dic = json.load(f)
+    for i in save_dic['pkgs']:
+        tmp_logs = [log for log in logs if log.startswith(i)]
+        random_100_dic[i] = tmp_logs
+    save_dic['appid_log_dic'] = random_100_dic
+    with open(f"/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/appid_file/random_{num}_no_error_appids.json", "w") as f:
+        json.dump(save_dic, f, indent = 2)
+        
 if __name__ == "__main__":
-    main()
+    # error_appids, no_error_appids, failure_appids, run_appids = main()
+    # update_log()
+    update_log(100)
    
         
     
