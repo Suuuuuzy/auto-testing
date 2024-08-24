f728d91893397d1271746e857b8580f022cbb9ad
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-08-24 18:27:50 -0400

    summarize results and randomly select

--- miniapp_data/utils/check_logs/get_appids.py
+++ miniapp_data/utils/check_logs/get_appids.py
@@ -21,6 +21,14 @@ def get_appids(appid_type):
         with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_plugin_list.log') as f:
             content = f.read()
         pattern = r'(wx[a-zA-Z0-9]{16}-pc) contains the two plugin'
+    elif appid_type=="noappjson_checked":
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_noappjson_list.log') as f:
+            content = f.read()
+        pattern = r'(wx[a-zA-Z0-9]{16}-pc)'
+    elif appid_type=="noappjson":
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_noappjson_list.log') as f:
+            content = f.read()
+        pattern = r'(wx[a-zA-Z0-9]{16}-pc) does not contain app.json'
         
     # Find all matches in the sample text
     matches = re.findall(pattern, content)
