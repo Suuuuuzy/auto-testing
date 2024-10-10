6344ddf2d028fd0ba7d6bb11129e5f2ebdcb9c3a
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-10-09 22:43:08 -0400

    reename some dirs

--- auto_minium/method_iterator/run_new.sh
+++ auto_minium/method_iterator/run_new.sh
@@ -1,3 +1,6 @@
 # generate index
-find /media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/newcrawl/pkg_unpack -maxdepth 1 -type d -name "wx*" > ../../miniapp_data/appid_file/newcrawl.txt
-./test/pipeline.py --input ../../miniapp_data/appid_file/newcrawl.txt
+# find /media/dataj/wechat-devtools-linux/testing/auto-testing/data/newcrawl/pkg_unpack -maxdepth 1 -type d -name "wx*" > ../../data/appid_file/newcrawl.txt
+cd /media/dataj/wechat-devtools-linux/testing/auto-testing/data/scripts
+./gen_newcrawl_run.py
+cd /media/dataj/wechat-devtools-linux/testing/auto-testing/auto_minium/method_iterator
+./test/pipeline.py --input ../../data/appid_file/newcrawl_run.txt
---
+++ data/data_utils/check_reports/get_appids.py
@@ -0,0 +1,46 @@
+import re
+
+def get_appids(appid_type):
+    if appid_type=='wxpay':
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_wxpay_list.log') as f:
+            content = f.read()
+        pattern = r'(wx[a-zA-Z0-9]{16}-pc) contains wxpay'
+    elif appid_type=='wxpay_checked':
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_wxpay_list.log') as f:
+            content = f.read()
+        pattern = r'wx[a-zA-Z0-9]{16}-pc'
+    elif appid_type=='decoded_checked':
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/decoded_check.log') as f:
+            content = f.read()
+        pattern = r'([a-zA-Z0-9_-]+)(?=\schecked)'
+    elif appid_type=="plugin_checked":
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_plugin_list.log') as f:
+            content = f.read()
+        pattern = r'wx[a-zA-Z0-9]{16}-pc'
+    elif appid_type=="plugin":
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_plugin_list.log') as f:
+            content = f.read()
+        pattern = r'(wx[a-zA-Z0-9]{16}-pc) contains the two plugin'
+    elif appid_type=="noappjson_checked":
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_noappjson_list.log') as f:
+            content = f.read()
+        pattern = r'(wx[a-zA-Z0-9]{16}-pc)'
+    elif appid_type=="noappjson":
+        with open('/media/dataj/wechat-devtools-linux/testing/auto-testing/miniapp_data/utils/check_logs/check_noappjson_list.log') as f:
+            content = f.read()
+        pattern = r'(wx[a-zA-Z0-9]{16}-pc) does not contain app.json'
+        
+    # Find all matches in the sample text
+    matches = re.findall(pattern, content)
+    matches = set(matches)    
+    return list(matches)
+
+import json
+def get_id_set_from_42w_file():
+    file = "/media/data4/jianjia_data4/miniapp_data/wxapkgs-42w.json"
+    with open(file) as f:
+        appids = json.load(f)
+    return set(appids)
+
+# appids = get_id_from_file()
+# print(appids[:100])
